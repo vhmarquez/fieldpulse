6e929162bc0ef0a76f941073c5ca8fa6f6222da5
Victor Marquez <victormarquez.dev@gmail.com>
2024-09-23 20:02:37 +0000

    Update to records

--- src/main.py
+++ src/main.py
@@ -2,12 +2,12 @@ from field_sure_database.upsert import db
 
 def main():
 
-    db(record_type='customers', limit=4, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
-    db(record_type='invoices', limit=4, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
-    db(record_type='jobs', limit=4, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
-    db(record_type='payments', limit=4, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
-    db(record_type='purchase_orders', limit=4, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
-    db(record_type='vendors', limit=4, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
+    db(record_type='customers', limit=5, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
+    db(record_type='invoices', limit=5, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
+    db(record_type='jobs', limit=5, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
+    db(record_type='payments', limit=5, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
+    db(record_type='purchase_orders', limit=5, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
+    db(record_type='vendors', limit=5, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
 
 if __name__ == '__main__':
     main()
