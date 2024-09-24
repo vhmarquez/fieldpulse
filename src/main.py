753e96b521be451a78030651aff92cbff1bd5505
Victor Marquez <victormarquez.dev@gmail.com>
2024-09-24 22:00:54 +0000

    Update to main functions

--- src/main.py
+++ src/main.py
@@ -3,12 +3,12 @@ from src.field_sure_database.connect import FieldSurgeDatabase
 
 def main():
 
-    db(record_type='customers', limit=5, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=True, delete_fact=False)
-    db(record_type='invoices', limit=5, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=True, delete_fact=False)
-    db(record_type='jobs', limit=5, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=True, delete_fact=False)
-    db(record_type='payments', limit=5, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=True, delete_fact=False)
-    db(record_type='purchase_orders', limit=5, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=True, delete_fact=False)
-    db(record_type='vendors', limit=5, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=True, delete_fact=False)
+    db(record_type='customers', limit=50, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
+    db(record_type='invoices', limit=50, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
+    db(record_type='jobs', limit=50, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
+    db(record_type='payments', limit=50, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
+    db(record_type='purchase_orders', limit=50, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
+    db(record_type='vendors', limit=50, max_pages=1, sort_by='updated_at', sort_dir='desc', upsert=True, delete_staging=False, delete_fact=False)
 
 if __name__ == '__main__':
     main()
