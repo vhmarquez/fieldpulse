8a46d21d756c87bb56bbf336311e904d7a18d97c
Victor Marquez <victormarquez.dev@gmail.com>
2024-09-24 17:51:26 +0000

    azure function

---
+++ function_app.py
@@ -0,0 +1,16 @@
+import logging
+import azure.functions as func
+
+from src import main
+
+app = func.FunctionApp()
+
+@app.schedule(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=True,
+              use_monitor=False) 
+def timer_trigger(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    main.main()
+
+    logging.info('Python timer trigger function executed.')
